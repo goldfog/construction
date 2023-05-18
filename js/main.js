4a9448929a91230533bb07632e77a16072b7f800
Dmitry <fogosony700@gmail.com>
2023-05-18 17:55:49 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.production__item').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    fade: true,
+    cssEase: 'linear'
+  });
+});
