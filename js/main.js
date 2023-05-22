0ce69eb383c49a02b167d5f40f2f3a3b58268222
Dmitry <fogosony700@gmail.com>
2023-05-22 14:42:37 +0300

    adaptive

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+const burger = document.querySelector(".menu__burger");
+const menu = document.querySelector(".header__nav");
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active");
+  menu.classList.toggle("active");
+});
+
+
 $(document).ready(function(){
   $('.production__item').slick({
     dots: false,
