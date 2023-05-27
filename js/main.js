75578306ef59536a550cbc6182069d498d434bc0
Dmitry <fogosony700@gmail.com>
2023-05-27 15:00:59 +0300

    swiper add

--- js/main.js
+++ js/main.js
@@ -7,71 +7,40 @@ burger.addEventListener("click", () => {
 });
 
 
-$(document).ready(function(){
-  $('.production__item').slick({
-    dots: false,
-    arrows: false,
+$('.reviews__items').slick({
     infinite: true,
-    speed: 500,
-    autoplay: false,
-    autoplaySpeed: 5000,
-    fade: true,
-    cssEase: 'linear',
+    slidesToShow: 3,
+    slidesToScroll: 3,
     responsive: [
+      {
+        breakpoint: 910,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          PrevArrows: "",
+          NextArrows: "",
+          dots: true
+        }
+      },
       {
         breakpoint: 768,
         settings: {
-          touchMove: true,
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 670,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true
         }
       }
     ]
   });
-});
 
-$('.reviews__items').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 3,
-  responsive: [
-    {
-      breakpoint: 910,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        infinite: true,
-        dots: true,
-        PrevArrows: "",
-        NextArrows: "",
-        dots: true
-      }
-    },
-    {
-      breakpoint: 768,
-      settings: {
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 670,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true
-      }
-    },
-    {
-      breakpoint: 670,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        dots: true,
-      }
-    }
-  ]
-});
+
